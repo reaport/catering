6e5d6756efecf83c69e22c68b39c71a288545f11
Evgeniy <120410817+nachos503@users.noreply.github.com>
2025-03-15 06:45:52 +0300

    Update UpdateConfigRequest.cs

--- CateringService/Models/UpdateConfigRequest.cs
+++ CateringService/Models/UpdateConfigRequest.cs
@@ -3,7 +3,6 @@
     public class UpdateConfigRequest
     {
         public int ConflictRetryCount { get; set; }
-        public double MovementSpeed { get; set; }
         public int NumberOfCateringVehicles { get; set; }
     }
 }
